52a3e3ef547539574fe15074059e431e2c8dab43
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2020-09-27 01:20:48 +0900

    select store

--- lotteOff/selectApp/views.py
+++ lotteOff/selectApp/views.py
@@ -3,9 +3,15 @@ from .models import Products
 from django.core.paginator import Paginator
 
 def select(request):
-    return render(request, 'select.html')
+    all_product = Products.objects.all()
 
-def detail(request, store_id):
-    store = get_object_or_404(Products, pk=store_id)
-    return render(request, 'detail_store.html', {'store':store})
 
+    return render(request, 'select.html', {'all_product': all_product})
+
+def detail_product(request, product_id):
+    store = get_object_or_404(Products, pk=product_id)
+    return render(request, 'detail_product.html', {'store':store})
+
+def storeFilter(request, storeName):
+    storePost = Products.objects.filter(storeName=storeName)
+    return render(request, 'detail_store.html', {"storePost":storePost, "storeName": storeName})
