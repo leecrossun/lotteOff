57007ef4143a8405f9a7007f730f88643ab63b68
oni <sion0210@naver.com>
2020-10-05 17:43:29 +0900

    selectApp에서 장바구니 버튼 연결

--- lotteOff/selectApp/urls.py
+++ lotteOff/selectApp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include   ### include 추가
 from . import views
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path('select/<str:region>', views.regionFilter, name="regionFilter"),
     path('detail_store/<str:storeName>', views.storeFilter, name="storeFilter"),
     path('detail_product/<int:product_id>', views.detail_product, name="detail_product"),
+    path('', include('cartApp.urls', namespace='cart')),   ### url 추가
 ]
